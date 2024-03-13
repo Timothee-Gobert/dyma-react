3e6b69c9a269a983cf5726a468caa6890d1f9bc2
Inox <timothee.gobert92@gmail.com>
2024-03-13 11:44:03 +0100

    Mise en place du contenue

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,24 @@
+import styles from "./Content.module.scss";
+
 function Content() {
-    return <div className="flex-fill debug-2">Content</div>
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+          <div className={styles.elem}>Elément</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
