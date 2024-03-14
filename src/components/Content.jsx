b5b24be476a3b415497296bced5e2d751af43edc
Inox <timothee.gobert92@gmail.com>
2024-03-14 19:20:16 +0100

    Ajouter des recettes dans une wishlist

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,18 @@
 import styles from "./Content.module.scss";
+import Recipe from "./Recipe";
+import { data } from "../data/recipes";
 
 function Content() {
+
+  const recipes = data;
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
-          <div className={styles.elem}>Elément</div>
+          {recipes.map((r) => (
+            <Recipe title={r.title} image={r.image} />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/data/recipes.js
@@ -0,0 +1,36 @@
+export const data = [
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+
+    {
+        title: "saumon et asperge",
+        image: "assets/images/Oeuf-au-plat.jpg"
+    },
+];
