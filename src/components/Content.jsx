e60346f0658193d6d6e66e3e828ea8f8b1778871
Inox <timothee.gobert92@gmail.com>
2024-03-15 03:43:15 +0100

    Mise en forme responsive

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,15 +3,15 @@ import Recipe from "./Recipe";
 import { data } from "../data/recipes";
 
 function Content() {
-
   const recipes = data;
+
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card p-20 br ${styles.contentCard}`}>
         <div className={styles.grid}>
           {recipes.map((r) => (
-            <Recipe title={r.title} image={r.image} />
+            <Recipe key={r._id} title={r.title} image={r.image} />
           ))}
         </div>
       </div>
