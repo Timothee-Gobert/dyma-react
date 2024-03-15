9581f40b232c5271aa18e1501711437890f6d433
Inox <timothee.gobert92@gmail.com>
2024-03-15 14:44:27 +0100

    Mise en place du menu deroulant sur petit ecran

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,34 @@
 import styles from "./Header.module.scss";
 
 import logoCookChef from "../assets/images/cookchef.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
-      <i className="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
         <img src={logoCookChef} alt="Logo du site" scale="1%" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-primary">
-          <i className="fa-solid fa-basket-shopping mr-5"></i>
-          <span>panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
         <button className="btn btn-reverse-primary">connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`${styles.headerXs} fa-solid fa-bars`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className={`calc`}></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
